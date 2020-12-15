8a4a28d8304d6f5becd14094b15943f8868b62ae
Gustavo Suto <guyrux@gmail.com>
2020-12-14 23:08:31 -0300

    [chore] First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to understand trends in decisions of collegiate judges in any region. It has only the intention of learning and uses open Brazilian data.',
+    author='Carne de Pescoço',
+    license='MIT',
+)
